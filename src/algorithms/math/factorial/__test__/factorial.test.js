6cfcdf5e8fcabc760763b87d0d0ef5c9f789e095
alperenmehmet <me.alperen@gmail.com>
2022-11-28 17:57:46 +0300

    factorial added.

---
+++ src/algorithms/math/factorial/__test__/factorial.test.js
@@ -0,0 +1,12 @@
+import { factorial } from '../factorial'
+
+describe('factorial', () => {
+  it('calculate correctly', () => {
+    expect(factorial(0)).toBe(1)
+    expect(factorial(1)).toBe(1)
+    expect(factorial(2)).toBe(2)
+    expect(factorial(3)).toBe(6)
+    expect(factorial(4)).toBe(24)
+    expect(factorial(5)).toBe(120)
+  })
+})
---
+++ src/algorithms/math/factorial/factorial.js
@@ -0,0 +1,7 @@
+export const factorial = (n) => {
+  let result = 1
+  for (let i = 2; i <= n; i++) {
+    result *= i
+  }
+  return result
+}
